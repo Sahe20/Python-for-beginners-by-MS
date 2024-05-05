476b083f5127ecd90965fb7b876d7801fd3cc35b
Sahe20 <jackitehm@gmail.com>
2024-05-05 16:28:38 +0300

    Working with booleans

--- practice.py
+++ practice.py
@@ -1,8 +1,14 @@
-                    #Variables
-x = 1 # assign variable x the value 1
-y = x + 5 #assign variable y the value of x plus 5
-z = y # assign variable z the value of y
-a = 3
-b = 3.0
-print(type(a))
-print(type(b))
+#                     #Variables
+# x = 1 # assign variable x the value 1
+# y = x + 5 #assign variable y the value of x plus 5
+# z = y # assign variable z the value of y
+# a = 3
+# b = 3.0
+# print(type(a))
+# print(type(b))
+
+                    # Working with Boolean
+x = True 
+y = False 
+print(type(x))
+print(type(y)) 
