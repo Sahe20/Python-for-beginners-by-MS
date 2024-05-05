cf7860c95136ea66b3bc5962f1128ebb6631490f
Sahe20 <jackitehm@gmail.com>
2024-05-05 16:21:17 +0300

    Understanding the basics of variables

---
+++ practice.py
@@ -0,0 +1,8 @@
+                    #Variables
+x = 1 # assign variable x the value 1
+y = x + 5 #assign variable y the value of x plus 5
+z = y # assign variable z the value of y
+a = 3
+b = 3.0
+print(type(a))
+print(type(b))
